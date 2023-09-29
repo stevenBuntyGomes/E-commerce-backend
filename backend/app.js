82bf1288e8aea1b80c89964c8e97c7b5c0f9fd08
stevenBuntyGomes <stevenBuntyGomes@users.noreply.github.com>
2023-09-29 14:59:09 +0600

    res.sendfile correction

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use("/api/v1", payment);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "https://adorable-nasturtium-445742.netlify.app/"));
 });
 // app.use(express.static(path.join(__dirname, "../frontend/build")));
 
